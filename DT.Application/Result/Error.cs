2cc17b0aa88c52de16d14c56e4fa3bc8a2b75492
Александр <@DESKTOP-L9V170T>
2025-10-04 16:48:04 +0300

    Добавьте файлы проекта.

---
+++ DT.Application/Result/Error.cs
@@ -0,0 +1,39 @@
+﻿namespace DT.Application.Result
+{
+    /// <summary>
+    /// Обхект ошибки
+    /// </summary>
+    public class Error
+    {
+        /// <summary>
+        /// Код ошибки
+        /// </summary>
+        public string Code { get; }
+
+        /// <summary>
+        /// Параметры
+        /// </summary>
+        public object[]? Parameters { get; }
+
+        /// <summary>
+        /// Представляет отсутствие ошибки.
+        /// </summary>
+        public static readonly Error None = new Error(string.Empty);
+
+        /// <summary>
+        /// Указывает, что ошибка отсутствует.
+        /// </summary>
+        public bool IsNone => this == None;
+
+        /// <summary>
+        /// Указывает, что у ошибки есть параметры для подстановки.
+        /// </summary>
+        public bool HasParameters => Parameters != null && Parameters.Length > 0;
+        public Error(string code, object[]? parameters = null)
+        {
+            Code = code;
+            Parameters = parameters;
+        }
+
+    }
+}
