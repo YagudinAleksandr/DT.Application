3ea3e257ed1e560809f38184ebdc75bea3070377
Александр <@DESKTOP-L9V170T>
2025-11-03 17:15:31 +0300

    Добавлен сервис для фонового запуска сервисов

---
+++ DT.Application/Services/IStartupRunnerService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace DT.Application
+{
+    /// <summary>
+    /// Сервис маркер для запуска сервисов в фоновом запуске
+    /// </summary>
+    public interface IStartupRunnerService
+    {
+        /// <summary>
+        /// Исполнить
+        /// </summary>
+        /// <param name="cancellationToken">Токен отмены</param>
+        Task ExecuteAsync(CancellationToken cancellationToken = default);
+    }
+}
